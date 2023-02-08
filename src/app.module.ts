160d33f2a7f0a49e787c4ef7884f347c1ddb3dc2
Leonardorossato <leonardorossato21@gmail.com>
2023-02-08 20:51:59 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
 import { ListModule } from './list/list.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [MovieModule, ListModule]
+  imports: [MovieModule, ListModule, AuthModule]
 })
 export class AppModule {}
