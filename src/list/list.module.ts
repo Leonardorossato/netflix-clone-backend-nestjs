1e9b0201eca9f7a0ea0e33943b20b313cfad2bc7
Leonardorossato <leonardorossato21@gmail.com>
2023-02-09 10:12:48 -0300

    update list.module.ts

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ListService } from './list.service';
 import { ListController } from './list.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { List, listSchema } from './schema/list.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: List.name, schema: listSchema }]),
+  ],
   controllers: [ListController],
-  providers: [ListService]
+  providers: [ListService],
 })
 export class ListModule {}
