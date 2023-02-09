90eafe81f786c28c31d916d305a2cbe1600cf479
Leonardorossato <leonardorossato21@gmail.com>
2023-02-09 09:38:27 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
 import { ListModule } from './list/list.module';
 import { AuthModule } from './auth/auth.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { mongoAsyncConnection } from './config/mongo.config';
 
 @Module({
-  imports: [MovieModule, ListModule, AuthModule]
+  imports: [
+    MovieModule,
+    ListModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync(mongoAsyncConnection),
+  ],
 })
 export class AppModule {}
