f9e16f271ee7eda8a688244ee66b2f70a4ce299b
Leonardorossato <leonardorossato21@gmail.com>
2023-02-09 10:12:57 -0300

    update movie.module.ts

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Movie, MovieSchema } from './schema/movie.shema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Movie.name, schema: MovieSchema }]),
+  ],
   controllers: [MovieController],
-  providers: [MovieService]
+  providers: [MovieService],
 })
 export class MovieModule {}
