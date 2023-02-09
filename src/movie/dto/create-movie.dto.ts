d6c7d89cb76f7a5aca3d86116514f0b7bb015b5e
Leonardorossato <leonardorossato21@gmail.com>
2023-02-09 10:17:48 -0300

    update create-movie.dto.ts

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,1 +1,12 @@
-export class CreateMovieDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateMovieDto {
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  title: string;
+
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  description: string;
+
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  img: string;
+}
