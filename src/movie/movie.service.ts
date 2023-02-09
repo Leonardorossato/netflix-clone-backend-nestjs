0a3f55a604980f8290da89ec4181bb0fe2af90f8
Leonardorossato <leonardorossato21@gmail.com>
2023-02-09 11:09:03 -0300

    update movie.service.ts

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateMovieDto } from './dto/create-movie.dto';
@@ -12,22 +12,68 @@ export class MovieService {
   ) {}
 
   async create(dto: CreateMovieDto) {
-    return 'This action adds a new movie';
+    try {
+      const movie = await this.movieModel.create(dto);
+      await movie.save();
+      return movie;
+    } catch (error) {
+      throw new HttpException('Error creating a movie', HttpStatus.BAD_REQUEST);
+    }
   }
 
   async findAll() {
-    return `This action returns all movie`;
+    try {
+      const movie = await this.movieModel.find();
+      return movie;
+    } catch (error) {
+      throw new HttpException(
+        'Error finding all movies',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findOne(id: string) {
-    return `This action returns a #${id} movie`;
+    try {
+      const movie = await this.movieModel.findOne({ where: { id: id } });
+      if (!movie)
+        throw new HttpException('Movie not found', HttpStatus.NOT_FOUND);
+      return movie;
+    } catch (error) {
+      throw new HttpException(
+        `Error to find a movie with ${id}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async update(id: string, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+    try {
+      const movie = await this.movieModel.findById({ id: id });
+      if (!movie)
+        throw new HttpException('Movie not found', HttpStatus.NOT_FOUND);
+      await this.movieModel.findByIdAndUpdate(id, updateMovieDto);
+      return { message: 'Movie updated successfully.' };
+    } catch (error) {
+      throw new HttpException(
+        `Error to update a movie with ${id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   async remove(id: string) {
-    return `This action removes a #${id} movie`;
+    try {
+      const movie = await this.movieModel.findById({ id: id });
+      if (!movie)
+        throw new HttpException('Movie not found', HttpStatus.NOT_FOUND);
+      await this.movieModel.findByIdAndUpdate(id);
+      return { message: 'Movie removed successfully.' };
+    } catch (error) {
+      throw new HttpException(
+        `Error to remove a movie with ${id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
